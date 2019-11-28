27c5c0a4498ed08c3ae3f15a8f38130f7885ef64
Rohmad Kurniadi <rohmad1230@gmail.com>
2019-11-28 10:17:04 +0700

    First commit to frontend branch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-teal-100">
     <Todo/>
   </div>
 </template>
--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="hello">
-    <ol>
-      <li v-for="list in lists" :key="list.id">
-        <h3>{{ list.title }}</h3>
-        <p>{{ list.content }}</p>
-      </li>
-    </ol>
+  <div class="container mx-auto py-20">
+      <div class="p-5 shadow-md bg-teal-400 text-center text-white mb-5">
+          <h1>Noted</h1>
+      </div>
+
+      <div class="p-10 shadow-md bg-white">
+        <div class="flex mb-5">
+            <div class="w-2/3">
+                <button class="mr-3 btn-primary">Add Task</button>
+                <button class="mr-3 btn-disabled">Delete Selected</button>
+                <button class="mr-3 btn-delete">Delete Selected</button>
+            </div>
+            <div class="w-1/3">
+                <input type="text" class="">
+            </div>
+        </div>
+
+        <ol>
+            <li v-for="list in lists" :key="list.id">
+                <h3>{{ list.title }}</h3>
+                <p>{{ list.content }}</p>
+            </li>
+        </ol>
+      </div>
   </div>
 </template>
 
@@ -22,7 +39,7 @@ export default {
     }
   },
   created() {
-    this.load()
+    // this.load()
   },
   methods: {
     load() {
